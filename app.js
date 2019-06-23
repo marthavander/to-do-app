77d78cfe6ca5c5eb6ec66254aa1858ff643ace5b
Martha <marthavander@gmail.com>
2019-06-23 15:50:17 -0500

    assignment 6

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
 function onReady() {
-     const toDos = [];
-       const addToDoForm = document.getElementById('addToDoForm');
-     function createNewToDo() {
-       const newToDoText = document.getElementById('newToDoText');
-       if (!newToDoText.value) { return; }
-       toDos.push({
-  title: newToDoText.value,
-  complete: false
+  const toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+    title: newToDoText.value,
+    complete: false
 });
-  newToDoText.value = '';
-  renderTheUI();
+    id++;
+    newToDoText.value = '';
+    renderTheUI();
 }
-function renderTheUI() {
-const toDoList = document.getElementById('toDoList');
-toDoList.textContent = '';
-toDos.forEach(function(toDo) {
-  const newLi = document.createElement('li');
-        const checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        newLi.textContent = toDo.title;
-        toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
+      function renderTheUI() {
+        const toDoList = document.getElementById('toDoList');
+        toDoList.textContent = '';
+
+        toDos.forEach(function(toDo) {
+          const newLi = document.createElement('li');
+          const checkbox = document.createElement('input');
+          checkbox.type = "checkbox";
+
+          const deleteBtn = document.createElement('button');
+          deleteBtn.textContent = 'DELETE';
+
+          deleteBtn.addEventListener('click', () => {
+            toDos = toDos.filter(function(item)) {
+              return item.id !==toDo.id;
+            })
+          renderTheUI();
+  });
+
+          newLi.textContent = toDo.title;
+          toDoList.appendChild(newLi);
+          newLi.appendChild(checkbox);
+          newLi.aooebdChild(deleteBtn);
    });
   }
 addToDoForm.addEventListener('submit', event => {
