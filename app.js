9cff4db6316f9a4cc5670c59fff55f9f28ed93a0
Martha VanderMeulen <48190780+marthavander@users.noreply.github.com>
2019-07-11 11:56:50 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 function onReady() {
-     const toDos = [];
-       const addToDoForm = document.getElementById('addToDoForm');
-     function createNewToDo() {
-       const newToDoText = document.getElementById('newToDoText');
-       if (!newToDoText.value) { return; }
-       toDos.push({
-  title: newToDoText.value,
-  complete: false
-});
-  newToDoText.value = '';
+  let toDos = [];
+  const addToDoForm = document.getElementById('addToDoForm');
+  let id = 0;
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      id: id,
+      title: newToDoText.value,
+      complete: false
+    });
+    id++;
+    newToDoText.value = '';
+    renderTheUI();
+  }
+      function renderTheUI() {
+        const toDoList = document.getElementById('toDoList');
+        toDoList.textContent = '';
+
+        toDos.forEach(function(toDo) {
+          const newLi = document.createElement('li');
+          const checkbox = document.createElement('input');
+          checkbox.type = "checkbox";
+
+          const deleteBtn = document.createElement('button');
+          deleteBtn.textContent = 'DELETE';
+
+          deleteBtn.addEventListener('click', event => {
+  toDos = toDos.filter(function (item) {
+    console.log('item', item);
+    return item.id !== toDo.id;
+  })
+  console.log('toDos', toDos);
   renderTheUI();
-}
-function renderTheUI() {
-const toDoList = document.getElementById('toDoList');
-toDoList.textContent = '';
-toDos.forEach(function(toDo) {
-  const newLi = document.createElement('li');
-        const checkbox = document.createElement('input');
-        checkbox.type = "checkbox";
-        newLi.textContent = toDo.title;
-        toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
+});
+
+          newLi.textContent = toDo.title;
+          toDoList.appendChild(newLi);
+          newLi.appendChild(checkbox);
+          newLi.appendChild(deleteBtn);
    });
   }
 addToDoForm.addEventListener('submit', event => {
